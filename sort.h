c7e8528582ecf6f9a7aa1aae5546c83e16d106dd
Ines Oubabas <143883134+Ines-Oubabas@users.noreply.github.com>
2024-01-16 14:11:15 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,14 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
-<<<<<<< HEAD
-
 #include <stdio.h>
 #include <stdlib.h>
-
-=======
 #include <stddef.h>
 #include <stdbool.h>
->>>>>>> ea77904187673509d5571df21f3d8d19289e6887
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -31,8 +26,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-<<<<<<< HEAD
-
-=======
->>>>>>> ea77904187673509d5571df21f3d8d19289e6887
 #endif
