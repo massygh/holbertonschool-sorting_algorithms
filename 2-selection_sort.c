706c3ec2af322f1367887362ec6dfd51e2538c98
Ines-Oubabase <inesoubabas56@gmail.com>
2024-01-17 12:41:34 +0100

    supprimer deux espaces

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,13 +16,11 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
-
 		if (min_idx != i)
 		{
 			temp = array[i];
